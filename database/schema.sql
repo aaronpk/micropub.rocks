8fc895e671cdab6b9c0f3908a245f52594841f49
Aaron Parecki <aaron@parecki.com>
2016-10-03 12:37:19 -0700

    update schema

--- database/schema.sql
+++ database/schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE `users` (
   `email` varchar(255) DEFAULT NULL,
   `auth_code` varchar(64) DEFAULT NULL,
   `auth_code_exp` datetime DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
